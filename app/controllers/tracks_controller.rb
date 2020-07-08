823d30482ead058753d3ce11d8011268cffc01cd
quinqu <quinruby0@gmail.com>
2020-07-08 10:20:34 -0700

    calcualte tempo based off mph and stride length formula

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,8 +1,6 @@
 class TracksController < ApplicationController
 
   def all_saved_tracks 
-    # find by id 
-    # 
     #user = User.first #give this to client, you dont want this for security reasons
     user = RSpotify::User.find(params[:id]) 
     #find a way to use token to find user
