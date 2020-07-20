598b9e291f32d718e105a506f53e66ab57aafff8
quinqu <quinruby0@gmail.com>
2020-07-20 14:41:58 -0700

    redirects with user URI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   
   def spotify
 
-    
     @spotify_user = Concurrent::Future.execute { RSpotify::User.new(request.env['omniauth.auth']) }
-    
-    redirect_to "heightScheme://#{@spotify_user.value.id}"
+    redirect_to "heightScheme://#{@spotify_user.value.uri}"
     return
   end 
 
 
 end
+
+
